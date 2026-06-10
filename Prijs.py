dc593c81f4a36897286203d59cb8be968187a08f
Xa4Go <111722@st-maartenscollege.nl>
2026-06-10 09:58:38 +0000

    Les 2

---
+++ Prijs.py
@@ -0,0 +1,10 @@
+
+Prijs = float(input("wat is de prijs van je lievelings drankje?"))
+Aantal = int(input("hoeveel drankjes wilt u kopen?"))
+Totaal = Prijs * Aantal
+print("de totale prijs is", Totaal)
+
+Prijs = float (input("wat is de prijs van je lievelings hapje?"))
+Aantal = int(input("hoeveel hapjes wilt u kopen?"))
+Totaal = Prijs * Aantal
+print("de totale prijs is", Totaal)
--- main.py
+++ main.py
@@ -1,8 +1,51 @@
-def bereken_prijs(aantal, prijs_per_stuk):
-    return aantal * prijs_per_stuk
+from jinja2.nodes import Break
+import random
+import os
 
-aantal = int(input("Hoeveel stuks wil je? "))
-prijs_per_stuk = float(input("Wat is de prijs per stuk? "))
+def scherm_wissen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-totaal = bereken_prijs(aantal, prijs_per_stuk)
-print("De totale prijs is: " + str(totaal))
+
+
+Woorden = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
+woord=random.choice(Woorden)
+geraden_letters = []
+beurten=5
+
+if beurten > 0:
+    print('welkom bij galgje')
+print('je hebt ' + str(beurten) + ' beurten')
+print('het woord heeft ' + str(len(woord)) + ' letters')
+
+
+
+tekst = input("Voer een tekst in: ")
+
+
+resultaat = ''
+for letter in woord:
+    if letter in geraden_letters:
+     resultaat += letter
+else:
+    resultaat += '-'
+
+    print('huidige stand van het woord ' + resultaat)
+if resultaat == woord:
+ print('gefeliciteerd je hebt het woord geraden')
+    
+ Break
+
+ gok = input('raad een letter ')
+ geraden_letters.append(gok)
+
+ if gok in woord:
+        print('goed geraden')
+else:
+        print('deze letter zit niet in het woord')
+        beurten = beurten - 1
+if beurten == 0 and resultaat != woord:
+     scherm_wissen()
+print('je hebt verloren het woord was ' + woord)
